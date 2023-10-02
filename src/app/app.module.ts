cdeee8d0516f4f071e2ed8885e8c5aff4b45d6df
R0b__zombie_31 <neagurobertalexandru@gmail.com>
2023-10-02 09:26:09 +0200

    iojiojoijoij

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { SezioneAnagraficaComponent } from './sezione-anagrafica/sezione-anagrafica.component';
+import { SezioneStudiComponent } from './sezione-studi/sezione-studi.component';
+import { SezioneLavoriComponent } from './sezione-lavori/sezione-lavori.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: SezioneAnagraficaComponent },
+      { path: 'sezione/studi', component: SezioneStudiComponent },
+      { path: 'sezione/lavori', component: SezioneLavoriComponent }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    SezioneAnagraficaComponent,
+    SezioneStudiComponent,
+    SezioneLavoriComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
---
+++ src/app/sezione-studi/sezione-studi.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sezione-studi',
+  templateUrl: './sezione-studi.component.html',
+  styleUrls: ['./sezione-studi.component.css']
+})
+export class SezioneStudiComponent {
+
+  share() {
+    window.alert('Sezione Studi');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
