c3d3cf1eed87cc2cd19a578fd7f163a50147e781
R0b__zombie_31 <neagurobertalexandru@gmail.com>
2023-10-02 11:14:32 +0200

    oninit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   datiAnagrafici=false;
   infoStudi=false;
   infoLavori=false;
+
+  constructor(){
+
+  }
+
+  ngOnInit(){
+
+  }
+
+  
 
 }
 
