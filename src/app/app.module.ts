d45f12b9671967ceb96e3c3126714465c2461bfd
R0b__zombie_31 <neagurobertalexandru@gmail.com>
2023-10-11 10:31:31 +0200

    creato-app-routing.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([
+    AppRoutingModule,
+    /* RouterModule.forRoot([
     //{ path: '', component:  },    
-    ]),
+    ]), */
     FormsModule,
   ],
   declarations: [
